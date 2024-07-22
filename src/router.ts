cff8ab21aebf9e96a3b376913d7294ddab693779
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-22 13:33:33 -0500

    move validation to router

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { createProducto } from './handlers/produc'
+import { body } from 'express-validator'
 
 const router = Router()
 
@@ -10,6 +11,17 @@ router.get('/hola' , (req , res) => {
 
 })
 
-router.post('/' , createProducto )
+router.post('/' ,
+    
+    // Validation
+    body('name')
+    .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
+
+    body('price')
+        .isNumeric().withMessage('valor no valido')
+        .notEmpty().withMessage('El precio de Producto no puede ir vacio')
+        .custom( value => value > 0).withMessage('Precio n ovalido'),
+
+    createProducto )
 
 export default router
