5c6872705148038bf58ed6c65ecbb7d2ccef2cc5
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-24 11:52:08 -0500

    add test to get response

--- src/handlers/__tests__/products.test.ts
+++ src/handlers/__tests__/products.test.ts
@@ -75,5 +75,12 @@ describe('POST /api/products',() => {
 describe('GET /api/products ' , () => { 
     it( 'get a JSON response with products ' , async () => { 
         const response  = await request(server).get('/api/products/')
+
+        expect(response.body).toHaveProperty('data')
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+
+        expect(response.status).not.toBe(404)
+
     })
 })
