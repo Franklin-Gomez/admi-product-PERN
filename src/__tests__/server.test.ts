b5b8712c3db4b655a805020ddb73b5c1c7e85136
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-23 20:24:58 -0500

    first test

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,6 @@
+//primera prueba
+describe('Nuestro primer test' , () => {  //  primer test , nombre del test
+    it('Debe revisar que 1 + 1 sean 2' , () => {  // descripcion del test
+        expect( 1 + 1).toBe( 2 ) // expect sera la prueba , tobe sera el resultado esperado
+    })
+})
