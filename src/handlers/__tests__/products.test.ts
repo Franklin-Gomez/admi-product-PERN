263d7e3642a23076849d5ded65192833ada093e9
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-24 10:12:26 -0500

    validate price greater than 0 and is a number

--- src/handlers/__tests__/products.test.ts
+++ src/handlers/__tests__/products.test.ts
@@ -17,6 +17,39 @@ describe('POST /api/products',() => {
         expect(response.body.errors).not.toHaveLength(2)
     })
 
+    it('should display price = 0 or negative' , async () => { 
+        const response = await request(server).post('api/products').send({
+            name : "Monitor Curvo",
+            price : 0
+        })
+
+        // debe ser igual
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body).toHaveLength(1)
+
+        // no debe ser igual
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    }) 
+
+    it('should display price is lowe than 0 or string' , async () => { 
+        const response = await request(server).post('api/products').send({
+            name : "Monitor Curvo",
+            price : 0
+        })
+
+        // debe ser igual
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body).toHaveLength(2)
+
+        // no debe ser igual
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(1)
+    }) 
+
+
 
     it('should create a new product' , async () => { 
         const response = await request(server).post('/api/products').send({
